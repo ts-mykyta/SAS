02c1007cd9813d8c9e45155c408c02a49ca27b40
Mykyta <m.paziuka@tsquad.net>
2025-09-30 18:02:03 +0300

    add activities pages

---
+++ src/components/activities/ActivitiesFilter.vue
@@ -0,0 +1,249 @@
+<script></script>
+
+<template>
+  <div class="md:w-72 shrink-0 block">
+    <div class="bg-card rounded-xl shadow-sm overflow-hidden border border-border">
+      <div class="p-5 border-b border-border">
+        <h3 class="text-lg font-semibold text-card-foreground">Filtry</h3>
+      </div>
+
+      <div data-state="open" class="border-b border-border">
+        <button
+          type="button"
+          aria-controls="radix-«r8j»"
+          aria-expanded="true"
+          data-state="open"
+          class="flex w-full items-center justify-between p-5 text-left font-medium hover:bg-muted text-card-foreground"
+        >
+          <span>Destynacje</span>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="lucide lucide-chevron-down h-5 w-5 transition-transform ui-open:rotate-180"
+            aria-hidden="true"
+          >
+            <path d="m6 9 6 6 6-6"></path>
+          </svg>
+        </button>
+        <div
+          data-state="open"
+          id="radix-«r8j»"
+          class="px-5 pb-5 space-y-2"
+          style="
+            transition-duration: 0s;
+            animation-name: none;
+            --radix-collapsible-content-height: 300px;
+            --radix-collapsible-content-width: 286.66668701171875px;
+          "
+        >
+          <div class="flex items-center space-x-2">
+            <button
+              type="button"
+              role="checkbox"
+              aria-checked="false"
+              data-state="unchecked"
+              value="on"
+              class="peer h-4 w-4 shrink-0 rounded-sm border border-primary ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground"
+              id="destination-Australia"
+            ></button
+            ><label
+              for="destination-Australia"
+              class="text-sm leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 cursor-pointer text-card-foreground"
+              >Australia</label
+            >
+          </div>
+          <div class="flex items-center space-x-2">
+            <button
+              type="button"
+              role="checkbox"
+              aria-checked="false"
+              data-state="unchecked"
+              value="on"
+              class="peer h-4 w-4 shrink-0 rounded-sm border border-primary ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground"
+              id="destination-Brazil"
+            ></button
+            ><label
+              for="destination-Brazil"
+              class="text-sm leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 cursor-pointer text-card-foreground"
+              >Brazylia</label
+            >
+          </div>
+          <div class="flex items-center space-x-2">
+            <button
+              type="button"
+              role="checkbox"
+              aria-checked="false"
+              data-state="unchecked"
+              value="on"
+              class="peer h-4 w-4 shrink-0 rounded-sm border border-primary ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground"
+              id="destination-Canada"
+            ></button
+            ><label
+              for="destination-Canada"
+              class="text-sm leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 cursor-pointer text-card-foreground"
+              >Kanada</label
+            >
+          </div>
+          <div class="flex items-center space-x-2">
+            <button
+              type="button"
+              role="checkbox"
+              aria-checked="false"
+              data-state="unchecked"
+              value="on"
+              class="peer h-4 w-4 shrink-0 rounded-sm border border-primary ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground"
+              id="destination-France"
+            ></button
+            ><label
+              for="destination-France"
+              class="text-sm leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 cursor-pointer text-card-foreground"
+              >Francja</label
+            >
+          </div>
+          <div class="flex items-center space-x-2">
+            <button
+              type="button"
+              role="checkbox"
+              aria-checked="false"
+              data-state="unchecked"
+              value="on"
+              class="peer h-4 w-4 shrink-0 rounded-sm border border-primary ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground"
+              id="destination-Iceland"
+            ></button
+            ><label
+              for="destination-Iceland"
+              class="text-sm leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 cursor-pointer text-card-foreground"
+              >Islandia</label
+            >
+          </div>
+          <div class="flex items-center space-x-2">
+            <button
+              type="button"
+              role="checkbox"
+              aria-checked="false"
+              data-state="unchecked"
+              value="on"
+              class="peer h-4 w-4 shrink-0 rounded-sm border border-primary ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground"
+              id="destination-Italy"
+            ></button
+            ><label
+              for="destination-Italy"
+              class="text-sm leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 cursor-pointer text-card-foreground"
+              >Włochy</label
+            >
+          </div>
+          <div class="flex items-center space-x-2">
+            <button
+              type="button"
+              role="checkbox"
+              aria-checked="false"
+              data-state="unchecked"
+              value="on"
+              class="peer h-4 w-4 shrink-0 rounded-sm border border-primary ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground"
+              id="destination-Japan"
+            ></button
+            ><label
+              for="destination-Japan"
+              class="text-sm leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 cursor-pointer text-card-foreground"
+              >Japonia</label
+            >
+          </div>
+          <div class="flex items-center space-x-2">
+            <button
+              type="button"
+              role="checkbox"
+              aria-checked="false"
+              data-state="unchecked"
+              value="on"
+              class="peer h-4 w-4 shrink-0 rounded-sm border border-primary ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground"
+              id="destination-Kenya"
+            ></button
+            ><label
+              for="destination-Kenya"
+              class="text-sm leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 cursor-pointer text-card-foreground"
+              >Kenia</label
+            >
+          </div>
+          <div class="flex items-center space-x-2">
+            <button
+              type="button"
+              role="checkbox"
+              aria-checked="false"
+              data-state="unchecked"
+              value="on"
+              class="peer h-4 w-4 shrink-0 rounded-sm border border-primary ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground"
+              id="destination-Morocco"
+            ></button
+            ><label
+              for="destination-Morocco"
+              class="text-sm leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 cursor-pointer text-card-foreground"
+              >Maroko</label
+            >
+          </div>
+          <div class="flex items-center space-x-2">
+            <button
+              type="button"
+              role="checkbox"
+              aria-checked="false"
+              data-state="unchecked"
+              value="on"
+              class="peer h-4 w-4 shrink-0 rounded-sm border border-primary ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground"
+              id="destination-New Zealand"
+            ></button
+            ><label
+              for="destination-New Zealand"
+              class="text-sm leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 cursor-pointer text-card-foreground"
+              >Nowa Zelandia</label
+            >
+          </div>
+          <div class="flex items-center space-x-2">
+            <button
+              type="button"
+              role="checkbox"
+              aria-checked="false"
+              data-state="unchecked"
+              value="on"
+              class="peer h-4 w-4 shrink-0 rounded-sm border border-primary ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground"
+              id="destination-Peru"
+            ></button
+            ><label
+              for="destination-Peru"
+              class="text-sm leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 cursor-pointer text-card-foreground"
+              >Peru</label
+            >
+          </div>
+          <div class="flex items-center space-x-2">
+            <button
+              type="button"
+              role="checkbox"
+              aria-checked="false"
+              data-state="unchecked"
+              value="on"
+              class="peer h-4 w-4 shrink-0 rounded-sm border border-primary ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground"
+              id="destination-Thailand"
+            ></button
+            ><label
+              for="destination-Thailand"
+              class="text-sm leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 cursor-pointer text-card-foreground"
+              >Tajlandia</label
+            >
+          </div>
+        </div>
+      </div>
+      <div class="p-5">
+        <button
+          class="w-full py-2 px-4 border border-border rounded-md text-sm font-medium text-card-foreground hover:bg-muted"
+        >
+          Resetuj filtry
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
