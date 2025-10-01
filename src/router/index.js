ddf177d8f8d5e1bce979a4847d646f9b873155c6
Mykyta <m.paziuka@tsquad.net>
2025-10-01 12:38:08 +0300

    add about page

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import ActivitiesView from '@/views/ActivitiesView.vue'
 import PrivacyView from '@/views/PrivacyView.vue'
 import TermsView from '@/views/TermsView.vue'
 import CookieView from '@/views/CookieView.vue'
+import AboutView from '@/views/AboutView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -42,10 +43,7 @@ const router = createRouter({
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      component: AboutView,
     },
   ],
 })
